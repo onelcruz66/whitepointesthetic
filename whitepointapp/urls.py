d1de1561f10dedeb07852db9b783b4876131e0f1
Cruz <Onel.Cruz@G3TI.Local>
2023-12-08 17:08:31 -0500

    Adding django url paths for all pages and base html.

--- whitepointapp/urls.py
+++ whitepointapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("services/", views.services, name="services"),
+    path("gallery/", views.gallery, name="gallery"),
+    path("contact/", views.contact, name="contact"),
+    path("appointment/", views.appointment, name="appointment")
 ]
--- whitepointapp/views.py
+++ whitepointapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def services(request):
+    return render(request, "services.html")
+
+def gallery(request):
+    return render(request, "gallery.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def appointment(request):
+    return render(request, "appointment.html")
